5a90377ce52a37872361c1bf776b26d4867b3952
asish012 <asish012@gmail.com>
2016-08-19 00:36:41 +0200

    refactor on variadict template argument unwrapping with initializer list

--- VariadicTemplate.cpp
+++ VariadicTemplate.cpp
@@ -11,22 +11,14 @@ std::string to_string_impl(T const& t) {
   return ss.str();
 }
 
-std::vector<std::string> to_string() { return {}; }
-
-template <typename P, typename ... Param>
-std::vector<std::string> to_string(P const& p, Param const& ... param) {
-  std::vector<std::string> results;
-  results.push_back( to_string_impl(p) );
-
-  std::vector<std::string> reminder = to_string(param ...);
-  results.insert( results.end(), reminder.begin(), reminder.end());
-
-  return results;
+template <typename ... Param>
+std::vector<std::string> to_string(Param const& ... param) {
+  return { to_string_impl(param)... };
 }
 
 int main() {
 
-  std::vector<std::string> res = to_string("hello", 12.6f, '*', "world");
+  std::vector<std::string> res = to_string("hello", 12.6f, '*', "foo");
 
   for (auto const& i: res) {
     std::cout << i << std::endl;
