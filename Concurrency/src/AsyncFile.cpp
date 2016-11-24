74ff6cb28e13dcdea1232cd503dcb5c2adf1691a
asish012 <asish012@gmail.com>
2016-11-24 08:05:16 +0100

    fix conflict

--- Concurrency/src/AsyncFile.cpp
+++ Concurrency/src/AsyncFile.cpp
@@ -42,13 +42,7 @@ void write() {
 }
 
 int main() {
-//    write();
+    write();
 
-    std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::string accumulated = std::accumulate(std::next(v.begin()), v.end(), std::to_string(*v.begin()),
-                                              [](std::string &init, int i){
-                                                  return init + "-" + std::to_string(i);
-                                              });
-    std::cout << accumulated << std::endl;
     return 0;
 }
